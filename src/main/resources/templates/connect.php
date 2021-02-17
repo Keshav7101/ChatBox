e5f2357b6a4e8516610b888bc3e42e8ce63651d0
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-02-17 13:08:51 +0530

    Connected the form page with the database.

--- src/main/resources/templates/connect.php
+++ src/main/resources/templates/connect.php
@@ -3,19 +3,29 @@ $Name = $_POST['Name'];
 $Email = $_POST['Email'];
 $Contact = $_POST['Contact'];
 
+//procedural approach
+$con=mysqli_connect("localhost","root","","ank");
+
+$query="INSERT INTO form(Name,Email,Contact)VALUES('$Name','$Email','$Contact')";
+if(mysqli_query($con,$query)){
+    header("refresh:1,url=form.html");
+}
+
 // Database connection
-$conn = new mysqli('test@localhost','root','','ank');
+/*$conn = new mysqli('localhost','root','','ank');
 if($conn->connect_error){
     echo "$conn->connect_error";
     die("Connection Failed : ". $conn->connect_error);
 }
 else {
-    $stmt = $conn->prepare("insert into registration(Name, Email, Contact) values(?, ?, ?)");
-    $stmt->bind_param("sssi", $Name, $Email, $Contact);
+
+
+    $stmt = $conn->prepare("insert into form(Name, Email, Contact) values(?, ?, ?)");
+    $stmt->bind_param("ssi", $Name, $Email, $Contact);
     $execval = $stmt->execute();
     echo $execval;
     echo "Registration successfully...";
     $stmt->close();
     $conn->close();
-}
+}*/
 ?>
